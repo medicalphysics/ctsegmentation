5f4064a773daf937377579589e17fcb50afb6e53
erlend andersen <andersen.erlend@gmail.com>
2024-04-30 14:58:31 +0200

    test

--- data/utilities/convert_tot_seg201_to_nnunet.py
+++ data/utilities/convert_tot_seg201_to_nnunet.py
@@ -1,9 +1,12 @@
-import pandas as pd
 import os
+import shutil
+import json
+from concurrent.futures import ThreadPoolExecutor
 import nibabel
 from tqdm import tqdm
 import numpy as np
-import shutil
+import pandas as pd
+
 from matplotlib import pylab as plt
 
 from segmentation_volumes import VOLUMES
@@ -88,24 +91,69 @@ def test_segmentation_overlap():
                     print("{} overlap {} with {} voxels".format(name1, name2, nvox))
         return
 
+def read_segmentation(path):
+    return nibabel.load(path).get_fdata().astype(np.uint8)
 
 
+def combine_segmentations(ct_path, subject_path, out_path):       
+    ct = nibabel.load(ct_path)
+    seg_map = np.zeros(ct.shape, dtype=np.uint8)            
+    jobs = list()
+    with ThreadPoolExecutor(max_workers=64) as executor:
+        for key, name in VOLUMES.items():
+            path = os.path.join(subject_path, "segmentations", f"{name}.nii.gz")
+            jobs.append((key, executor.submit(read_segmentation, path)))
+    
+    for key, fut in tqdm(jobs, leave=False):
+        seg_map[fut.result() > 0] = key 
+    nibabel.save(nibabel.Nifti1Image(seg_map, ct.affine), out_path)
+
+
+def create_json(subjects):        
+    subjects_train = list([s for s, split in subjects if split == 'train'])
+    subjects_val = list([s for s, split in subjects if split == 'val'])
+    
+    json_dict = {}
+    json_dict['name'] = "CTQA_TotalSegmentator"
+    json_dict['channel_names'] = {"0": "CT"}
+    
+    json_dict['labels'] = {val: idx for idx, val in (VOLUMES | {0:'background'}).items()}
+    json_dict['numTraining'] = len(subjects_train) + len(subjects_val)
+    json_dict['file_ending'] = '.nii.gz'
+    json_dict['overwrite_image_reader_writer'] = 'NibabelIOWithReorient'
+
+    with open(os.path.join(NNUNET_FOLDERPATH, "dataset.json"), "w") as fil:
+        json.dump(json_dict, fil, sort_keys=False, indent=4)
+    
+    splits = []
+    splits.append({
+        "train": subjects_train,
+        "val": subjects_val
+    })
+    with open(os.path.join(NNUNET_FOLDERPATH, "splits_final.json"), "w") as fil:
+        json.dump(splits, fil, sort_keys=False, indent=4)
 
 
 def create_nnunet_dataset():
-    subjects = read_subjects()  
-      
-    for subject, split in subjects:
-        subject_path = os.path.join(NNUNET_FOLDERPATH, )
-
-
-
-
-
-
-
-
+    os.makedirs(os.path.join(NNUNET_FOLDERPATH, "imagesTr"), exist_ok=True)
+    os.makedirs(os.path.join(NNUNET_FOLDERPATH, "labelsTr"), exist_ok=True)
+    os.makedirs(os.path.join(NNUNET_FOLDERPATH, "imagesTs"), exist_ok=True)
+    os.makedirs(os.path.join(NNUNET_FOLDERPATH, "labelsTs"), exist_ok=True)
+    subjects = read_subjects()            
+    create_json(subjects)
+    for subject, split in tqdm(subjects):
+        subject_path = os.path.join(TOTSEG_FOLDERPATH, subject)
+        ct_path = os.path.join(subject_path, "ct.nii.gz")
+        
+        ## copy ct image
+        if split == "train" or split == "val":            
+            shutil.copy(ct_path, os.path.join(NNUNET_FOLDERPATH, "imagesTr", f"{subject}_000.nii.gz"))
+            combine_segmentations(ct_path, subject_path, os.path.join(NNUNET_FOLDERPATH, "labelsTr", f"{subject}.nii.gz"))            
+        else:
+            shutil.copy(ct_path, os.path.join(NNUNET_FOLDERPATH, "imagesTs", f"{subject}_000.nii.gz"))
+            combine_segmentations(ct_path, subject_path, os.path.join(NNUNET_FOLDERPATH, "labelsTs", f"{subject}.nii.gz"))            
 
 if __name__ == '__main__':
-    test_segmentation_files()
+    #test_segmentation_files()
     #test_segmentation_overlap()
+    create_nnunet_dataset()
