cd4e476183852d9ed18d901955b99e61dd55bdf4
erlend andersen <andersen.erlend@gmail.com>
2024-05-03 14:35:18 +0200

    test

--- data/utilities/convert_tot_seg201_to_nnunet.py
+++ data/utilities/convert_tot_seg201_to_nnunet.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import json
-from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
 import nibabel
 from tqdm import tqdm
 import numpy as np
@@ -12,7 +12,7 @@ from matplotlib import pylab as plt
 from segmentation_volumes import VOLUMES
 
 TOTSEG_FOLDERPATH = r"C:\Users\ander\totseg\Totalsegmentator_dataset_v201"
-NNUNET_FOLDERPATH = r"C:\Users\ander\totseg\nnunet_data"
+NNUNET_FOLDERPATH = r"C:\Users\ander\totseg\nnUNet_raw\Dataset001_DXMC"
 
 def read_subjects():
     meta = pd.read_csv(os.path.join(TOTSEG_FOLDERPATH, "meta.csv"), sep=';')
@@ -91,15 +91,15 @@ def test_segmentation_overlap():
                     print("{} overlap {} with {} voxels".format(name1, name2, nvox))
         return
 
+
 def read_segmentation(path):
     return nibabel.load(path).get_fdata().astype(np.uint8)
-
 
 def combine_segmentations(ct_path, subject_path, out_path):       
     ct = nibabel.load(ct_path)
     seg_map = np.zeros(ct.shape, dtype=np.uint8)            
     jobs = list()
-    with ThreadPoolExecutor(max_workers=64) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=64) as executor:
         for key, name in VOLUMES.items():
             path = os.path.join(subject_path, "segmentations", f"{name}.nii.gz")
             jobs.append((key, executor.submit(read_segmentation, path)))
@@ -114,7 +114,7 @@ def create_json(subjects):
     subjects_val = list([s for s, split in subjects if split == 'val'])
     
     json_dict = {}
-    json_dict['name'] = "CTQA_TotalSegmentator"
+    json_dict['name'] = "DXMC"
     json_dict['channel_names'] = {"0": "CT"}
     
     json_dict['labels'] = {val: idx for idx, val in (VOLUMES | {0:'background'}).items()}
@@ -134,6 +134,18 @@ def create_json(subjects):
         json.dump(splits, fil, sort_keys=False, indent=4)
 
 
+def create_patient(subject, split):     
+    subject_path = os.path.join(TOTSEG_FOLDERPATH, subject)
+    ct_path = os.path.join(subject_path, "ct.nii.gz")        
+    ## copy ct image
+    if split == "train" or split == "val":            
+        shutil.copy(ct_path, os.path.join(NNUNET_FOLDERPATH, "imagesTr", f"{subject}_0000.nii.gz"))
+        combine_segmentations(ct_path, subject_path, os.path.join(NNUNET_FOLDERPATH, "labelsTr", f"{subject}.nii.gz"))            
+    else:
+        shutil.copy(ct_path, os.path.join(NNUNET_FOLDERPATH, "imagesTs", f"{subject}_0000.nii.gz"))
+        combine_segmentations(ct_path, subject_path, os.path.join(NNUNET_FOLDERPATH, "labelsTs", f"{subject}.nii.gz"))
+
+
 def create_nnunet_dataset():
     os.makedirs(os.path.join(NNUNET_FOLDERPATH, "imagesTr"), exist_ok=True)
     os.makedirs(os.path.join(NNUNET_FOLDERPATH, "labelsTr"), exist_ok=True)
@@ -141,17 +153,9 @@ def create_nnunet_dataset():
     os.makedirs(os.path.join(NNUNET_FOLDERPATH, "labelsTs"), exist_ok=True)
     subjects = read_subjects()            
     create_json(subjects)
+   
     for subject, split in tqdm(subjects):
-        subject_path = os.path.join(TOTSEG_FOLDERPATH, subject)
-        ct_path = os.path.join(subject_path, "ct.nii.gz")
-        
-        ## copy ct image
-        if split == "train" or split == "val":            
-            shutil.copy(ct_path, os.path.join(NNUNET_FOLDERPATH, "imagesTr", f"{subject}_000.nii.gz"))
-            combine_segmentations(ct_path, subject_path, os.path.join(NNUNET_FOLDERPATH, "labelsTr", f"{subject}.nii.gz"))            
-        else:
-            shutil.copy(ct_path, os.path.join(NNUNET_FOLDERPATH, "imagesTs", f"{subject}_000.nii.gz"))
-            combine_segmentations(ct_path, subject_path, os.path.join(NNUNET_FOLDERPATH, "labelsTs", f"{subject}.nii.gz"))            
+        create_patient(subject, split)
 
 if __name__ == '__main__':
     #test_segmentation_files()
