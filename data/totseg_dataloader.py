fa53f50088aa4643b2e40fb9e9216670983cf099
Erlend Andersen <andersen.erlend@gmail.com>
2024-05-11 12:51:12 +0200

    test

--- data/totseg_dataloader.py
+++ data/totseg_dataloader.py
@@ -13,16 +13,13 @@ class CustomImageDataset(Dataset):
     def __init__(self, data_dir: str):        
         patients = os.listdir(data_dir)
         self._item_paths = list([os.path.join(data_dir, p) for p in patients if os.path.isdir(os.path.join(data_dir, p))])
-        self.prepare_labels()
+#        self.prepare_labels()
 
 
 
-
-    def prepare_labels(self, overwrite=False):
-        """Make labels array"""
-        print("Making labels arrays")
-        def load_labels(path, overwrite=False):
-            if not os.path.exists(os.path.join(path, "labels.nii.gz")):
+    def load_labels(self, path, overwrite=False):
+        if not os.path.exists(os.path.join(path, "labels.nii.gz")):
+            if overwrite:
                 ct = nibabel.load(os.path.join(path, "ct.nii.gz"))
                 labels = np.zeros(ct.shape, dtype=np.float32)
                 for key, name in tqdm(VOLUMES.items(), leave=False):
@@ -31,9 +28,14 @@ class CustomImageDataset(Dataset):
                 nibabel.save(nibabel.Nifti1Image(labels, ct.affine), os.path.join(path, "labels.nii.gz")) 
 
 
-        with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:                        
+    def prepare_labels(self, overwrite=False):
+        """Make labels array"""
+        print("Making labels arrays")
+        
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()-2) as executor:                        
             pbar = tqdm(total=len(self._item_paths))
-            futures = list([executor.submit(load_labels, p, overwrite) for p in self._item_paths])
+            futures = list([executor.submit(self.load_labels, p, overwrite) for p in self._item_paths])
             for _ in concurrent.futures.as_completed(futures):
                 pbar.update(1)
             pbar.close()
@@ -43,8 +45,11 @@ class CustomImageDataset(Dataset):
 
     def __getitem__(self, idx):
         image = torch.as_tensor(nibabel.load(os.path.join(self._item_paths[idx], "ct.nii.gz")).get_fdata())
+        self.load_labels(self._item_paths[idx])
         label = torch.as_tensor(nibabel.load(os.path.join(self._item_paths[idx], "labels.nii.gz")).get_fdata())        
         return image, label
     
 if __name__ == '__main__':
     d = CustomImageDataset(r"/home/erlend/Totalsegmentator_dataset_v201")
+    for image, label in d:
+        print (image.shape, image.spa)
